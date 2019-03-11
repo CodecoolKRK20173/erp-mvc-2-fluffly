f41958eaa457a057f4a0ed81215d68108bf9a14c
PrzemekKondziela <kondziela.p@gmail.com>
2019-03-11 14:25:17 +0100

    crm menu

--- controller/crm_controller.py
+++ controller/crm_controller.py
@@ -13,4 +13,26 @@ def run():
         None
     """
 
-    # your code
+    options = ["Add new record to table",
+               "Remove a record with a given id from the table.",
+               "Updates specified record in the table.",
+               "What is the id of the customer with the longest name?",
+               "Which customers has subscribed to the newsletter?om_date < sale_date < to_date)"]
+
+    choice = None
+    while choice != "0":
+        choice = terminal_view.get_choice(options)
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            pass
+        else:
+            terminal_view.print_error_message("There is no such choice.")
