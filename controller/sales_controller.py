93f27d62576d5dfd150e70327969244bcc45d403
PrzemekKondziela <kondziela.p@gmail.com>
2019-03-11 14:16:50 +0100

    sale menu

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -13,4 +13,26 @@ def run():
         None
     """
 
-    # your code
+    options = ["Add new record to table",
+               "Remove a record with a given id from the table.",
+               "Updates specified record in the table.",
+               "Question: What is the id of the item that was sold for the lowest price?",
+               "Which items are sold between two given dates? (from_date < sale_date < to_date)"]
+
+    choice = None
+    while choice != "0":
+        choice = terminal_view.get_choice(options)
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            pass
+        else:
+            terminal_view.print_error_message("There is no such choice.")
