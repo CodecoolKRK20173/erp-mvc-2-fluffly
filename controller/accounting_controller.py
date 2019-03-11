6545a0323208fa20d66462f09a407ed3b6a348c8
PrzemekKondziela <kondziela.p@gmail.com>
2019-03-11 14:26:52 +0100

    accountig menu

--- controller/accounting_controller.py
+++ controller/accounting_controller.py
@@ -13,4 +13,26 @@ def run():
         None
     """
 
-    # your code
+    options = ["Add new record to table",
+               "Remove a record with a given id from the table.",
+               "Updates specified record in the table.",
+               "Which year has the highest profit? (profit = in - out)",
+               "What is the average (per item) profit in a given year? [(profit)/(items count)]"]
+
+    choice = None
+    while choice != "0":
+        choice = terminal_view.get_choice(options)
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            pass
+        else:
+            terminal_view.print_error_message("There is no such choice.")
