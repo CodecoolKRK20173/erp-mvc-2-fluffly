7e8f2e038d14b88fc30e664d616a916a576a99cf
PrzemekKondziela <kondziela.p@gmail.com>
2019-03-11 14:22:16 +0100

    inv menu

--- controller/hr_controller.py
+++ controller/hr_controller.py
@@ -13,4 +13,26 @@ def run():
         None
     """
 
-    # your code
+    options = ["Add new record to table",
+               "Remove a record with a given id from the table.",
+               "Updates specified record in the table.",
+               "Who is the oldest person?",
+               "Who is the closest to the average age?"]
+
+    choice = None
+    while choice != "0":
+        choice = terminal_view.get_choice(options)
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            pass
+        else:
+            terminal_view.print_error_message("There is no such choice.")
